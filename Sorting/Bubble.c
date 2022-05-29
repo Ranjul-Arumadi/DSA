3676021d23627066635d09d05bb1618864d6bf6e
Ranjul_Arumadi <54582161+Ranjul-Arumadi@users.noreply.github.com>
2022-05-29 18:53:56 +0530

    Update Bubble.c

--- Sorting/Bubble.c
+++ Sorting/Bubble.c
@@ -1,4 +1,6 @@
 /*
+The bubble sort gets its name because elements tend to move up into the correct order like bubbles rising to the surface.
+
 Bubble sort may be defined as the sorting algorithm that follows the approach of replacing the value in the first index with the smallest value in the array and keep it repeating until the list is sorted. It is a very simple way of performing sorting. In this way to sort the array, the value has to be assigned to the array in the beginning before starting the sorting.
 
  Below is the program to sort the array using bubble sort where the values have been taken from the user. Once the program is compiled and run, it will ask the user for the number of elements that they want to sort. Once the number is provided, the program will ask the user to provide values equivalent to the count that they have provided. The values will be stored in the array and will be processed further using nested for loop together with decision making using “if” in order to sort the array.
